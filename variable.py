6d3930ce27e3cfbe7fa377854bbb2a7431dd9a77
Pradeep-Battula <blnpradeep146@gmail.com>
2025-08-30 18:01:33 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,21 @@
+#A variable in Python is a name or label that refers to data stored in memory. It's a container that stores a value, which could be a number, string, or even more complex data like lists and dictionaries.
+#Examples of valid variable names:
+x, name, my_variable, userAge
+#Examples of invalid variable names:
+123abc (cannot start with a number), for (reserved keyword)
+
+#Example:
+                                          
+age = 25
+name = "Alice"
+height = 5.6
+
+#Example of Variable Usage:
+# Assigning values to variables
+length = 10
+width = 5
+area = length * width  # Performing a calculation using variables
+
+# Displaying the result
+print("The area is:", area)  
+# Output: The area is: 50

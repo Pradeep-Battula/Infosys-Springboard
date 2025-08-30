a4f17b510579e02f75c2220d920463a7bde175c8
Pradeep-Battula <blnpradeep146@gmail.com>
2025-08-30 23:07:47 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,14 @@
+# Factorial program in Python
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+n = int(input("Enter a number: "))
+
+if n < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    print(f"The factorial of {n} is {factorial(n)}")
